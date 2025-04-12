5a3fe060d4ce48b306f9bbde10d9c911f05a2e67
Thomas-Mewily <tstd36@gmail.com>
2025-04-12 14:04:08 +0200

    save

--- hexga/hexga_io/src/save.rs
+++ hexga/hexga_io/src/save.rs
@@ -16,7 +16,7 @@ impl Io
     ];
 }
 
-/* 
+
 #[allow(unused_variables)]
 #[allow(async_fn_in_trait)]
 // An abstraction over Serde tat support custom file format
@@ -31,43 +31,44 @@ pub trait IoSave<M> where Self: Sized + Serialize + for<'de> Deserialize<'de>, M
     /// Dedicated file extension to load the value. ex `png`, `jpeg` for image
     /// 
     /// Don't include the markup language extension like `json` or `ron`
-    fn io_open_own_extension() -> impl Iterator<Item=&'static str> { Self::BasedOn::io_open_own_extension() }
+    fn open_custom_extension() -> impl Iterator<Item=&'static str> { Self::BasedOn::open_custom_extension() }
 
     /// Dedicated file extension to load the value. ex `png`, `jpeg` for image
     /// 
     /// Don't include the markup language extension like `json` or `ron`
-    fn io_can_open_own_extension(extension : &path) -> bool { Self::io_open_own_extension().position(|e| e == extension).is_some() }
+    fn can_be_open_from_custom_extension(extension : &path) -> bool { Self::open_custom_extension().position(|e| e == extension).is_some() }
 
     /// Also include the markup language extension like `json` or `ron`
-    fn io_open_extension() -> impl Iterator<Item=&'static str> { Self::io_open_own_extension().chain(Io::MARKUP_LANGAGE_EXTENSION) }
+    fn open_extension() -> impl Iterator<Item=&'static str> { Self::open_custom_extension().chain(Io::MARKUP_LANGAGE_EXTENSION) }
     /// Also include the markup language extension like `json` or `ron`
-    fn io_can_open_extension(extension : &path) -> bool { Self::io_open_extension().position(|e| e == extension).is_some() }
+    fn can_be_open_from_extension(extension : &path) -> bool { Self::open_extension().position(|e| e == extension).is_some() }
 
     async fn from_file_composite(path : &path) -> Result<Self,M::Err>
     {
         match Box::pin(Self::BasedOn::from_file_composite(path)).await
         {
-            Ok(base) => Self::from_based_on(base).map_err(|e| IoError::new(path.to_owned(), IoErrorReason::Raw(e))),
+            Ok(base) => Self::from_based_on(base),
             Err(e) => Err(e),
         }
     }
 
     
-    fn from_bytes_with_extension(raw : &[u8], extension : &str) -> Result<Self,M::Err> 
+    fn from_bytes_with_extension(data : &[u8], extension : &str) -> Result<Self,M::Err> 
     {
         match extension
         {
-            "ron"  => Self::from_ron_buf (raw),
-            "json" => Self::from_json_buf(raw),
-            "xml"  => Self::from_xml_buf (raw),
-            "yaml" | "yml" => Self::from_yaml_buf(raw),
-
-            _ => match Self::BasedOn::from_bytes_with_extension(raw, extension)
+            /* 
+            "ron"  => Self::from_ron_buf (data),
+            "json" => Self::from_json_buf(data),
+            "xml"  => Self::from_xml_buf (data),
+            "yaml" | "yml" => Self::from_yaml_buf(data),
+            */
+            _ => match Self::BasedOn::from_bytes_with_extension(data, extension)
             {
                 Ok(base) => Self::from_based_on(base),
-                Err(_) => match Self::io_can_open_own_extension(extension)
+                Err(_) => match Self::can_be_open_from_custom_extension(extension)
                 {
-                    true => Self::from_bytes_with_own_extension(raw, extension),
+                    true => Self::from_bytes_with_own_extension(data, extension),
                     false => Self::result_err(format!("Can't load {} from extension .{}", std::any::type_name::<Self>(), extension))
                 },
             }
@@ -79,7 +80,12 @@ pub trait IoSave<M> where Self: Sized + Serialize + for<'de> Deserialize<'de>, M
         Self::result_err(format!("can't open composite {} from bytes", std::any::type_name::<Self>()))
     }
 
-    fn from_based_on(base : Self::BasedOn) -> Result<Self, M::Err> { Err(format!("can't open composite {} from bytes", std::any::type_name::<Self>())) }
+    fn from_based_on(base : Self::BasedOn) -> Result<Self, M::Err> { Self::result_err(format!("Can't open composite {} from bytes", std::any::type_name::<Self>())) }
+
+
+
+
+
 }
 
 pub(crate) trait InnerSave<M> where Self: Sized + Serialize + for<'de> Deserialize<'de>, M : IoMediator
@@ -89,5 +95,3 @@ pub(crate) trait InnerSave<M> where Self: Sized + Serialize + for<'de> Deseriali
 }
 
 impl<T,M> InnerSave<M> for T where T : IoSave<M>, M : IoMediator {}
-
-*/
