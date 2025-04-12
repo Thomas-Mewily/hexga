133f22f1a69cab100c2c06e8cf2d38309a3ecafd
Thomas-Mewily <tstd36@gmail.com>
2025-04-12 11:28:52 +0200

    ctx in io mediator direct

--- hexga/hexga_io/src/mediator/direct.rs
+++ hexga/hexga_io/src/mediator/direct.rs
@@ -1,40 +1,42 @@
 use crate::*;
 
-pub struct IoMediatorDirect<O,E> where O : IoOk, E : IoError
+pub struct IoMediatorDirect<Ctx,O,E> where O : IoOk, E : IoError
 {
-    read   : fn(&path, &mut Vec<u8>) -> Result<O,E>,
-    write  : fn(&path, &[u8]) -> Result<O,E>,
-    remove : fn(&path) -> Result<O,E>,
-    append : Option<fn(&path, &[u8]) -> Result<O,E>>,
+    context : Ctx,
+    read   : fn(&path, &mut Vec<u8>, &mut Ctx) -> Result<O,E>,
+    write  : fn(&path, &[u8], &mut Ctx) -> Result<O,E>,
+    remove : fn(&path, &mut Ctx) -> Result<O,E>,
+    append : Option<fn(&path, &[u8], &mut Ctx) -> Result<O,E>>,
 }
 
-impl Default for IoMediatorDirect<IoDiskOk,IoDiskError> where IoDiskOk : IoOk, IoDiskError : IoError
+impl Default for IoMediatorDirect<(),IoDiskOk,IoDiskError> where IoDiskOk : IoOk, IoDiskError : IoError
 {
     fn default() -> Self 
     {
         Self 
         { 
-            read: |path, buf| Io::disk_read_buf(path, buf), 
-            write: |path,data| Io::disk_write(path, data), 
-            remove: |path| Io::disk_remove(path),
-            append: Some(|path, data| Io::disk_append(path, data))
+            context: (),
+            read: |path, buf,_| Io::disk_read_buf(path, buf), 
+            write: |path,data,_| Io::disk_write(path, data), 
+            remove: |path,_| Io::disk_remove(path),
+            append: Some(|path, data,_| Io::disk_append(path, data)),
         }
     }
 }
 
-impl<O,E> IoMediator for IoMediatorDirect<O,E> where O : IoOk, E : IoError
+impl<Ctx,O,E> IoMediator for IoMediatorDirect<Ctx,O,E> where O : IoOk, E : IoError
 {
     type Ok=O;
     type Err=E;
 
     fn write(&mut self, path : &path, data : &[u8]) -> Result<Self::Ok,Self::Err> 
-    { (self.write)(path,data) }
+    { (self.write)(path,data,&mut self.context) }
 
     fn append(&mut self, path : &path, data : &[u8]) -> Result<Self::Ok,Self::Err> 
     { 
         match &self.append
         {
-            Some(append) => append(path, data),
+            Some(append) => append(path, data, &mut self.context),
             None => 
             {
                 let mut file_data = self.read(path)?;
@@ -45,7 +47,7 @@ impl<O,E> IoMediator for IoMediatorDirect<O,E> where O : IoOk, E : IoError
     }
 
     fn remove(&mut self, path : &path) -> Result<Self::Ok,Self::Err> {
-        (self.remove)(path)
+        (self.remove)(path, &mut self.context)
     }
 
     fn apply(&mut self) -> Result<Self::Ok,Self::Err> {
@@ -58,7 +60,7 @@ impl<O,E> IoMediator for IoMediatorDirect<O,E> where O : IoOk, E : IoError
     
     fn read(&mut self, path : &path) -> Result<Vec<u8>,Self::Err> {
         let mut data = Vec::with_capacity(2048);
-        (self.read)(path,&mut data)?;
+        (self.read)(path,&mut data, &mut self.context)?;
         Ok(data)
     }
 }
