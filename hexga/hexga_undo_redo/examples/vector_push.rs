d189de37a7fd77c6850d1cd51639f7f20d9c4cb6
Thomas-Mewily <tstd36@gmail.com>
2025-04-27 12:56:12 +0200

    fixed example

--- hexga/hexga_undo_redo/examples/vector_push.rs
+++ hexga/hexga_undo_redo/examples/vector_push.rs
@@ -3,18 +3,18 @@ use hexga_undo_redo::prelude::*;
 fn main()
 {
     let mut v = vec![];
-    let mut undo_action = vec![];
+    let mut actions = vec![];
 
-    v.push_action("foo", &mut undo_action);
-    v.push_action("bar", &mut undo_action);
+    v.push_action("foo", &mut actions);
+    v.push_action("bar", &mut actions);
 
     println!("before:");
     println!("vector: {:?}", v);
-    println!("undo_action: {:?}", undo_action);
+    println!("undo_action: {:?}", actions);
     println!();
 
-    v.undo_action(undo_action.pop().unwrap());
+    v.undo(&mut actions);
     println!("after:");
     println!("vector: {:?}", v);
-    println!("undo_action: {:?}", undo_action);
+    println!("undo_action: {:?}", actions);
 }
