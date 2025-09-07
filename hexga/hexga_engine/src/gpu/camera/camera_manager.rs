8d416639be7869eb740cdce21866b749d326b7fb
Mewily <tstd36@yahoo.fr>
2025-09-07 22:30:51 +0200

    _

--- hexga/hexga_engine/src/gpu/camera/camera_manager.rs
+++ hexga/hexga_engine/src/gpu/camera/camera_manager.rs
@@ -46,7 +46,7 @@ impl CameraManager
         let m = self.matrix();
         //info!("pushed matrix");
         //info!("{}", m);
-        Gpu.base.queue.write_buffer(&Cam.camera_buffer, 0, m.as_u8_slice());
+        Gpu.queue.write_buffer(&Cam.camera_buffer, 0, m.as_u8_slice());
     }
 }
 impl GetMatrix<float,4,4> for CameraManager
