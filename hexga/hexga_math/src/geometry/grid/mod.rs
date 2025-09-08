08938f229ff38755988c1d9fc96806c4c2c7d029
Mewily <tstd36@yahoo.fr>
2025-09-09 00:14:54 +0200

    _

--- hexga/hexga_math/src/geometry/grid/mod.rs
+++ hexga/hexga_math/src/geometry/grid/mod.rs
@@ -6,13 +6,19 @@ mod serde_impl;
 #[cfg(feature = "serde")]
 pub use serde_impl::*;
 
-mod typedef;
+pub mod typedef;
 mod grid;
+pub use grid::*;
 mod igrid;
+pub use igrid::*;
 mod view;
+pub use view::*;
 mod iter;
+pub use iter::*;
 mod view_mut;
+pub use view_mut::*;
 mod iter_mut;
+pub use iter_mut::*;
 
 pub mod prelude
 {
