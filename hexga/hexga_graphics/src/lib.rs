433cbed01036e94af06cb0406223cbb42fd1532d
Mewily <tstd36@yahoo.fr>
2025-12-11 18:36:32 +0100

    hexga graphics format

--- hexga/hexga_graphics/src/lib.rs
+++ hexga/hexga_graphics/src/lib.rs
@@ -1,4 +1,10 @@
+#![allow(dead_code)]
+#![allow(unused)]
+use hexga::prelude::*;
+pub(crate) type GpuVertexBufferLayout<'a> = wgpu::VertexBufferLayout<'a>;
+
 pub use hexga_wgpu::*;
+mod format;
 
 pub mod prelude
 {
