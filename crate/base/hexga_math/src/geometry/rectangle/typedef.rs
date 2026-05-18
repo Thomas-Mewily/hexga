ed7d74bcf56107ee30092f00fd7f90b35e30e6e9
Thomas-Mewily <thomas.mewily@gmail.com>
2026-05-19 00:31:33 +0200

    rect typedef

--- crate/base/hexga_math/src/geometry/rectangle/typedef.rs
+++ crate/base/hexga_math/src/geometry/rectangle/typedef.rs
@@ -9,6 +9,8 @@ pub type Rect1 = Rectangle1<float>;
 pub const fn rect1(pos_x: float, size_x: float) -> Rect1 { rectangle1(pos_x, size_x) }
 pub type Rect1i = Rectangle1<int>;
 pub const fn rect1i(pos_x: int, size_x: int) -> Rect1i { rectangle1(pos_x, size_x) }
+pub type Rect1u = Rectangle1<uint>;
+pub const fn rect1u(pos_x: uint, size_x: uint) -> Rect1u { rectangle1(pos_x, size_x) }
 
 pub type Rectangle2<T> = Rectangle<T, 2>;
 pub const fn rectangle2<T>(pos_x: T, pos_y: T, size_x: T, size_y: T) -> Rectangle2<T>
@@ -22,6 +24,12 @@ pub const fn rect2(pos_x: float, pos_y: float, size_x: float, size_y: float) -> 
 }
 pub type Rect2i = Rectangle2<int>;
 pub const fn rect2i(pos_x: int, pos_y: int, size_x: int, size_y: int) -> Rect2i
+{
+    rectangle2(pos_x, pos_y, size_x, size_y)
+}
+
+pub type Rect2u = Rectangle2<uint>;
+pub const fn rect2u(pos_x: uint, pos_y: uint, size_x: uint, size_y: uint) -> Rect2u
 {
     rectangle2(pos_x, pos_y, size_x, size_y)
 }
@@ -62,6 +70,19 @@ pub const fn rect3i(
     size_y: int,
     size_z: int,
 ) -> Rect3i
+{
+    rectangle3(pos_x, pos_y, pos_z, size_x, size_y, size_z)
+}
+
+pub type Rect3u = Rectangle3<uint>;
+pub const fn rect3u(
+    pos_x: uint,
+    pos_y: uint,
+    pos_z: uint,
+    size_x: uint,
+    size_y: uint,
+    size_z: uint,
+) -> Rect3u
 {
     rectangle3(pos_x, pos_y, pos_z, size_x, size_y, size_z)
 }
@@ -108,6 +129,20 @@ pub const fn rect4i(
     size_z: int,
     size_w: int,
 ) -> Rect4i
+{
+    rectangle4(pos_x, pos_y, pos_z, pos_w, size_x, size_y, size_z, size_w)
+}
+pub type Rect4u = Rectangle4<uint>;
+pub const fn rect4u(
+    pos_x: uint,
+    pos_y: uint,
+    pos_z: uint,
+    pos_w: uint,
+    size_x: uint,
+    size_y: uint,
+    size_z: uint,
+    size_w: uint,
+) -> Rect4u
 {
     rectangle4(pos_x, pos_y, pos_z, pos_w, size_x, size_y, size_z, size_w)
 }
