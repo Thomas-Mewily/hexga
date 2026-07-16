76bcb3b5248631a2fc608635b1c58569162b6cd6
Thomas-Mewily <thomas.mewily@gmail.com>
2026-07-17 01:37:27 +0200

    math_tmp example

--- tmp/math_tmp/src/main.rs
+++ tmp/math_tmp/src/main.rs
@@ -6,8 +6,8 @@ fn main()
     // Not possible in rust
     // let x = [1u8, 2u8] as [f32;2];
 
-    let _ : [f32;2] = [1u8, 2u8].cast_into(); // manually typing it
-    let _ = [1u8, 2u8].to_f32();
+    let _ : [f32;2] = [1u8, 2u8].cast_into(); // Need to manually type the binding
+    let _ = [1u8, 2u8].to_f32(); // ok
 
     assert_eq!(
         rgba(1.0f32, 0.5, 0.25, 0.).to_u8_range(), 
